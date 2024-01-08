937924dbf85c0c29a24c8227286f98f059025bc2
Irfan010-i <128881141+Irfan010-i@users.noreply.github.com>
2024-01-08 19:56:09 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EDUWEB</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0
+,100;0,200;0,300;0,400;1,600;1,700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css" integrity="sha384-b6lVK+yci+bfDmaY1u0zE8YYJt0TZxLEAFyYSLHId4xoVvsrQu3INevFKo+Xir8e" crossorigin="anonymous">
+</head>
+
+<body>
+    <!-- --------HOME PAGE-------- -->
+    <section class="header">
+        <nav>
+            <a href="index.html"><img src="images/logo.png"></a>
+            <div class="nav-links">
+                <ul>
+                    <li><a href="index.html">HOME</li>
+                    <li><a href="about.html">ABOUT</a></li>
+                    <li><a href="course.html">COURSE</a></li>
+                    <li><a href="blog.html">BLOG</a></li>
+                    <li><a href="contact.html">CONTACT</a></li>
+                </ul>
+            </div>
+        </nav>
+    </section>
+    <section class="home">
+        <h1>WORLDS BiGGEST COLLEGE</h1><br>
+        <h3>Welcome to the college Welcome to the collegeWelcome to the college</h3>
+        <div class="btn">
+            click here to know more
+        </div>
+    </section>
+    <!-- --------COURSE-------- -->
+    <section class="course">
+        <h1>Courses We Offer</h1>
+        <p>We offer a wide variety of courses to our students</p>
+        <section class="row">
+            <div class="course-col">
+                <h3>Intermediate</h3>
+                <p>Lorem ipsum dolor sit amet. Quo tempora quia non corporis s
+                    int sit quae quia ut vitae doloremque ut dignissimos velit rem rerum
+                    eveniet ut suscipit perferendis! Est dolor rerum et illo aliquam est esse
+                    distinctio et doloribus nihil eos tempora consequuntur eum expedita nihil
+                    quo consequatur blanditiis? Et velit provident qui assumenda minima id
+                    dignissimos obcaecati ut numquam minima rem velit sapiente!</p>
+            </div>
+            <div class="course-col">
+                <h3>Degree</h3>
+                <p>Lorem ipsum dolor sit amet. Quo tempora quia non corporis s
+                    int sit quae quia ut vitae doloremque ut dignissimos velit rem rerum
+                    eveniet ut suscipit perferendis! Est dolor rerum et illo aliquam est esse
+                    distinctio et doloribus nihil eos tempora consequuntur eum expedita nihil
+                    quo consequatur blanditiis? Et velit provident qui assumenda minima id
+                    dignissimos obcaecati ut numquam minima rem velit sapiente!</p>
+            </div>
+            <div class="course-col">
+                <h3>Post-Graduate</h3>
+                <p>Lorem ipsum dolor sit amet. Quo tempora quia non corporis s
+                    int sit quae quia ut vitae doloremque ut dignissimos velit rem rerum
+                    eveniet ut suscipit perferendis! Est dolor rerum et illo aliquam est esse
+                    distinctio et doloribus nihil eos tempora consequuntur eum expedita nihil
+                    quo consequatur blanditiis? Et velit provident qui assumenda minima id
+                    dignissimos obcaecati ut numquam minima rem velit sapiente!</p>
+            </div>
+        </section>
+    </section>
+    <!-- --------  CAMPUS  -------- -->
+    <section class="campus">
+        <h1>Our Global Campus</h1>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <div class="row">
+            <div class="campus-col">
+                <img src="images/london.png">
+                <div class="layer">
+                    <h3>LONDON</h3>
+                </div>
+            </div>
+            <div class="campus-col">
+                <img src="images/newyork.png">
+                <div class="layer">
+                    <h3>NEW YORK</h3>
+                </div>
+            </div>
+            <div class="campus-col">
+                <img src="images/washington.png">
+                <div class="layer">
+                    <h3>WASHINGTON</h3>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- --------  FACILITIES  -------- -->
+    <section class="facilities">
+        <h1>Our Facilities</h1>
+        <p>Lorem ipsum dolor sit amet.</p>
+
+        <div class="row">
+            <div class="facilities-col">
+                <img src="images/library.png">
+                <h3>World Class Library</h3>
+                <p>Lorem ipsum dolor sit amet. Quo tempora quia non corporiss
+                    int sit quae quia ut vitae doloremque ut dignissimos velit rem rerum
+                    eveniet ut suscipit perferendis!</p>
+            </div>
+            <div class="facilities-col">
+                <img src="images/basketball.png">
+                <h3>Largest Playground</h3>
+                <p>Lorem ipsum dolor sit amet. Quo tempora quia non corporiss
+                    int sit quae quia ut vitae doloremque ut dignissimos velit rem rerum
+                    eveniet ut suscipit perferendis!</p>
+            </div>
+            <div class="facilities-col">
+                <img src="images/cafeteria.png">
+                <h3>Best Food Available</h3>
+                <p>Lorem ipsum dolor sit amet. Quo tempora quia non corporiss
+                    int sit quae quia ut vitae doloremque ut dignissimos velit rem rerum
+                    eveniet ut suscipit perferendis!</p>
+            </div>
+        </div>
+    </section>
+    <!-- --------  TESTIMONIALS  -------- -->
+    <section class="testimonials">
+        <h1>What Our Students Say</h1>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <div class="row">
+            <div class="test-col">
+                <img src="images/user1.jpg">
+                <div>
+                    <p>Lorem ipsum dolor sit amet. Quo tempora quia non corporiss
+                        int sit quae quia ut vitae doloremque ut dignissimos velit rem rerum
+                        eveniet ut suscipit perferendis!</p>
+                        <h3>Hannah</h3>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-half"></i>
+                    </div>
+                </div>
+            <div class="test-col">
+                <img src="images/user2.jpg">
+                <div>
+                    <p>Lorem ipsum dolor sit amet. Quo tempora quia non corporiss
+                        int sit quae quia ut vitae doloremque ut dignissimos velit rem rerum
+                        eveniet ut suscipit perferendis!</p>
+                        <h3>Clay</h3>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- --------  CALL TO ACTION  -------- -->
+    <section class="cta">
+        <h1>Enroll For Our Various Online Courses <br> Anywhere From The World</h1>
+        <a href="" class="btn">CONTACT US</a>
+    </section>
+
+    <!-- --------  FOOTER  -------- -->
+    <section class="footer">
+    <h4>About Us</h4>
+    <p>Lorem ipsum dolor sit amet. Quo tempora quia non corporiss
+        int sit quae quia ut vitae doloremque ut dignissimos velit rem rerum
+        eveniet ut suscipit perferendis! <br>
+        Lorem ipsum dolor sit amet. Quo tempora quia non corporiss</p>
+        <div class="icons">
+            <i class="bi bi-facebook"></i>
+            <i class="bi bi-instagram"></i>
+            <i class="bi bi-twitter"></i>
+            <i class="bi bi-linkedin"></i>
+            <p>Made with <i class="bi bi-heart"> </i> Irfan Shaikh</p>
+        </div>
+    </section>
+</body>
+</html>
